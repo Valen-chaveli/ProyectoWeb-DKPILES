52cd1a0704f0a6a1812e6b7d252033a712306f12
Valentin Chaveli <valen7est@gmail.com>
2021-02-14 23:02:18 +0100

    Agregado la seccion de club y la de alumnes para desktop y para movil

---
+++ assets/js/station_meteo.js
@@ -0,0 +1,13 @@
+function llamarWidget() {
+  WgsWidget({
+    id_station: 51,
+    wj: 'knots',
+    tj: 'c',
+    tmprh: true,
+    date_format: 'Y-m-d H:i:s T',
+    divid: 'wgs_widget_51_1403353076147',
+    type: 'curr',
+  });
+}
+llamarWidget();
+setInterval(llamarWidget, 60000); /* Quitar si no queréis que actualice */
